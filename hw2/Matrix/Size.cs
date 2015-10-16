5d3f5cf3a6c6283a43d9b01acaa9fd2f092b42c9
update rails --no-ri --no-rdoc <sergey.voronov@gmail.com>
2015-10-16 13:52:48 +0300

    matrix added code for size class

---
+++ hw2/Matrix/Size.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Matrix
+{
+    public class Size
+    {
+        public int Width
+        {
+            get { return _width; }
+            set { _width = value; }
+        }
+
+        public int Height
+        {
+            get { return _height; }
+            set { _height = value; }
+        }
+
+        private int _height;
+        private int _width;
+        public bool IsSquare;
+
+        public Size(int height, int width)
+        {
+            _height = height;
+            _width = width;
+            IsSquare = (_height == _width) ? true : false;
+        }
+
+        public static bool operator  == (Size s1, Size s2)
+        {
+            if (s1.Width == s2.Width && s1.Height == s2.Height) return true;
+            else
+                return false;
+        }
+
+        public static bool operator !=(Size s1, Size s2)
+        {
+            if (s1.Width != s2.Width || s1.Height != s2.Height)
+                return true;
+            else
+                return false;
+        }
+    }
+}
