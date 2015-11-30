542390c637bcb25fe3d88e725e3230bbf5e608f8
update rails --no-ri --no-rdoc <sergey.voronov@gmail.com>
2015-11-30 11:40:16 +0200

    kottans linq sum done

--- hw5/Kottans.LINQ/UserSum.cs
+++ hw5/Kottans.LINQ/UserSum.cs
@@ -35,16 +35,14 @@ namespace Kottans.LINQ
             return result;
         }
 
-
-
-        public static int Sum<TSource>(this IEnumerable<TSource> source, Func<TSource, int> predicate)
+       public static int Sum<TSource>(this IEnumerable<TSource> source, Func<TSource, int> predicate)
         {
             if (source == null) throw new ArgumentNullException();
             if (predicate == null) throw new ArgumentNullException();
 
             return source.Select(predicate).Sum();
         }
-        //
+
         public static int? Sum<TSource>(this IEnumerable<TSource> source, Func<TSource, int?> predicate)
         {
             if (source == null) throw new ArgumentNullException();
@@ -52,5 +50,199 @@ namespace Kottans.LINQ
 
             return source.Select(predicate).Sum();
         }
+
+        public static long Sum(this IEnumerable<long> source)
+        {
+            if (source == null) throw new ArgumentNullException();
+
+            var result = 0L;
+            checked
+            {
+                foreach (var i in source)
+                {
+                    result += i;
+                }
+            }
+
+            return result;
+        }
+
+        public static long? Sum(this IEnumerable<long?> source)
+        {
+            if (source == null) throw new ArgumentNullException();
+
+            var result = 0L;
+            checked
+            {
+                foreach (var i in source)
+                {
+                    result += i.GetValueOrDefault();
+                }
+            }
+
+            return result;
+        }
+
+        public static long Sum<TSource>(this IEnumerable<TSource> source, Func<TSource, long> predicate)
+        {
+            if (source == null) throw new ArgumentNullException();
+            if (predicate == null) throw new ArgumentNullException();
+
+            return source.Select(predicate).Sum();
+        }
+
+        public static long? Sum<TSource>(this IEnumerable<TSource> source, Func<TSource, long?> predicate)
+        {
+            if (source == null) throw new ArgumentNullException();
+            if (predicate == null) throw new ArgumentNullException();
+
+            return source.Select(predicate).Sum();
+        }
+
+        public static decimal Sum(this IEnumerable<decimal> source)
+        {
+            if (source == null) throw new ArgumentNullException();
+
+            var result = 0M;
+            checked
+            {
+                foreach (var i in source)
+                {
+                    result += i;
+                }
+            }
+
+            return result;
+        }
+
+        public static decimal? Sum(this IEnumerable<decimal?> source)
+        {
+            if (source == null) throw new ArgumentNullException();
+
+            var result = 0M;
+            checked
+            {
+                foreach (var i in source)
+                {
+                    result += i.GetValueOrDefault();
+                }
+            }
+
+            return result;
+        }
+
+        public static decimal Sum<TSource>(this IEnumerable<TSource> source, Func<TSource, decimal> predicate)
+        {
+            if (source == null) throw new ArgumentNullException();
+            if (predicate == null) throw new ArgumentNullException();
+
+            return source.Select(predicate).Sum();
+        }
+
+        public static decimal? Sum<TSource>(this IEnumerable<TSource> source, Func<TSource, decimal?> predicate)
+        {
+            if (source == null) throw new ArgumentNullException();
+            if (predicate == null) throw new ArgumentNullException();
+
+            return source.Select(predicate).Sum();
+        }
+
+        public static float Sum(this IEnumerable<float> source)
+        {
+            if (source == null) throw new ArgumentNullException();
+
+            var result = 0D;
+            checked
+            {
+                foreach (var i in source)
+                {
+                    result += i;
+                }
+            }
+
+            return (float)result;
+        }
+
+        public static float? Sum(this IEnumerable<float?> source)
+        {
+            if (source == null) throw new ArgumentNullException();
+
+            var result = 0D;
+            checked
+            {
+                foreach (var i in source)
+                {
+                    result += i.GetValueOrDefault();
+                }
+            }
+
+            return (float)result;
+        }
+
+        public static float Sum<TSource>(this IEnumerable<TSource> source, Func<TSource, float> predicate)
+        {
+            if (source == null) throw new ArgumentNullException();
+            if (predicate == null) throw new ArgumentNullException();
+
+            return source.Select(predicate).Sum();
+        }
+
+        public static float? Sum<TSource>(this IEnumerable<TSource> source, Func<TSource, float?> predicate)
+        {
+            if (source == null) throw new ArgumentNullException();
+            if (predicate == null) throw new ArgumentNullException();
+
+            return source.Select(predicate).Sum();
+        }
+
+        public static double Sum(this IEnumerable<double> source)
+        {
+            if (source == null) throw new ArgumentNullException();
+
+            var result = 0D;
+            checked
+            {
+                foreach (var i in source)
+                {
+                    result += i;
+                }
+            }
+
+            return result;
+        }
+
+        public static double? Sum(this IEnumerable<double?> source)
+        {
+            if (source == null) throw new ArgumentNullException();
+
+            var result = 0D;
+            checked
+            {
+                foreach (var i in source)
+                {
+                    result += i.GetValueOrDefault();
+                }
+            }
+
+            return result;
+        }
+
+        public static double Sum<TSource>(this IEnumerable<TSource> source, Func<TSource, double> predicate)
+        {
+            if (source == null) throw new ArgumentNullException();
+            if (predicate == null) throw new ArgumentNullException();
+
+            return source.Select(predicate).Sum();
+        }
+
+        public static double? Sum<TSource>(this IEnumerable<TSource> source, Func<TSource, double?> predicate)
+        {
+            if (source == null) throw new ArgumentNullException();
+            if (predicate == null) throw new ArgumentNullException();
+
+            return source.Select(predicate).Sum();
+        }
+       
+
     }
 }
